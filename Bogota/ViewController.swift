8874ee7f904588e97ce098a59184f81760faa32d
hoxvi <octxvi@poliot.co.kr>
2022-01-24 15:17:26 +0900

    Create project

---
+++ Bogota/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bogota
+//
+//  Created by 전현성 on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
